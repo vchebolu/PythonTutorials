dc225c0348476910ceba2937ae5a07453ca0842f
vchebolu <chebolu.sarma@gmail.com>
2023-01-19 19:47:47 +0530

    file compress functionality

---
+++ examples/zipfile.py
@@ -0,0 +1,13 @@
+import myzipfile
+import pathlib
+
+
+def make_archive(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir, "compressed.zip")
+    with myzipfile.ZipFile(dest_path,'w') as archive:
+        for filepath in filepaths:
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath,arcname=filepath.name)
+
+if __name__ == "__main__":
+    make_archive(filepaths=["todos.txt", "file.txt"], dest_dir="dest")
