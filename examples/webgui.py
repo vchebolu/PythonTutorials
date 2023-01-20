c8b89e4140097779ee3936b9e4423b60e2a8b15a
vchebolu <chebolu.sarma@gmail.com>
2023-01-20 17:34:43 +0530

    Implement Edit feature to webapp

--- examples/webgui.py
+++ examples/webgui.py
@@ -14,10 +14,16 @@ st.title("My Todo App")
 st.subheader("This is my todo App")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="new todo item",placeholder="Add newtodo...",
               on_change=add_todo, key='new_todo')
 
+st.session_state
 print("Hello")
