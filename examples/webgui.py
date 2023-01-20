a3f4b4c84c7ff5016bae296e864032856cf0b0f6
vchebolu <chebolu.sarma@gmail.com>
2023-01-20 17:18:24 +0530

    Implement Add new feature to webapp

--- examples/webgui.py
+++ examples/webgui.py
@@ -1,14 +1,23 @@
 import streamlit as st
 import functions
 
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    print(todo)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo App")
 st.write("This app is to increase your productivity")
 
-todos = functions.get_todos()
-
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add newtodo")
+st.text_input(label="new todo item",placeholder="Add newtodo...",
+              on_change=add_todo, key='new_todo')
+
+print("Hello")
